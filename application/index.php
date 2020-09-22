9cc09b1500639ccac549dfd537f20fdfcf6ea459
Andrey Alekseev <60799652+Alcotras71@users.noreply.github.com>
2020-09-22 13:42:30 +0300

    bitrix

---
+++ application/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оставить заявку");
+?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ include/logo_bottom.php
@@ -0,0 +1,1 @@
+<img src="<?=SITE_TEMPLATE_PATH?>/img/Footer/logo.png" alt="лого" />
